29fb6e0f1274fe19f117fb2ffde22b8c5e704f1e
joqtan18 <joqtan.jr@gmail.com>
2020-03-22 18:10:42 -0500

    avances

---
+++ app/Http/Controllers/AgenteController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AgenteController extends Controller
+{
+    public function index()
+    {
+        $data = DB::table('incidencia')
+                    ->join('trabajador','trabajador.trab_id','incidencia.inc_agente')
+                    ->get();
+        return view('incidencia.index',['incidencia'=>$data]);
+    }
+
+    public function create()
+    {
+        return view('incidencia.create');
+    }
+
+    public function store(Request $request)
+    {
+        // $this->validate($request,[
+        //     'apod_dni' => 'required|unique:apoderado,apod_dni|numeric|digits:8',
+        //     'apod_ape' => 'required|max:50|regex:/^[\pL\s\-]+$/u',
+        //     'apod_nom' => 'required|max:50|regex:/^[\pL\s\-]+$/u',
+        //     'apod_sexo' => 'required',
+        //     'apod_email' => 'nullable',
+        //     'apod_tel' => 'nullable|min:7|max:13'
+        // ]);
+        $data = $request->all();
+        $inc = Incidencia::create($data);
+        return redirect()->route('incidencia.index')->with('status', 'Incidencia agregado correctamente!');
+    }
+
+    public function show(Apoderado $apoderado)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        $apoderado = Apoderado::find($id);
+        return view ('apoderado.edit',['apod'=>$apoderado]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $apoderado = Apoderado::find($id);
+        $request->all();
+        $apoderado->update($request->all());
+        return redirect()->route('apoderado.index')->with('status', 'Apoderado editado correctamente!');
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        $apoderado = Apoderado::destroy($id);
+        return redirect()->route('apoderado.index')->with('status', 'Apoderado eliminado correctamente!');
+    }
+
+    public function consultarApod($dni){
+        $apoderado = DB::table('apoderado')
+                        ->where('apoderado.apod_dni','=',$dni)
+                        ->get();
+        return $apoderado;
+    }
+
+}
